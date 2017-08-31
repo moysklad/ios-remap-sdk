4d78a8d36d63850e2754b0a0ef2631a5e63315f8
Vladislav Ivanov <vladislav.ivanov.v@gmail.com>
2017-08-31 10:30:29 +0300

    MI-809

---
+++ MoySkladSDK/DataManager/DataManager+LoadTemplates.swift
@@ -0,0 +1,98 @@
+//
+//  DataManager+LoadTemplates.swift
+//  MoyskladiOSRemapSDK
+//
+//  Created by Vladislav on 31.08.17.
+//  Copyright © 2017 Andrey Parshakov. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+extension DataManager {
+    /**
+     Load print templates for document type.
+     Also see [ API reference](https://online.moysklad.ru/api/remap/1.1/doc/index.html#шаблон-печатной-формы)
+     - parameter forDocType: Document type for which templates should be loaded
+     - parameter auth: Authentication information
+     - parameter type: Type of Template that should be loaded
+     */
+    public static func templates<T: MSGeneralDocument>(forDocType: T.Type, auth: Auth, type: MSTemplateType) -> Observable<[MSEntity<MSTemplate>]> {
+        return HttpClient.get(loadUrlTemplate(type: forDocType), auth: auth, urlPathComponents: [type.rawValue])
+            .flatMapLatest { result -> Observable<[MSEntity<MSTemplate>]> in
+                guard let result = result else {
+                    return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrectTemplateResponse.value))
+                }
+                
+                let deserialized = result.msArray("rows").map { MSTemplate.from(dict: $0) }
+                let withoutNills = deserialized.removeNils()
+                
+                guard withoutNills.count == deserialized.count else {
+                    return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrectTemplateResponse.value))
+                }
+                return Observable.just(withoutNills)
+        }
+    }
+    
+    /**
+     Load link to created PDF for document from template.
+     Also see [ API reference](https://online.moysklad.ru/api/remap/1.1/doc/index.html#печать-документов)
+     - parameter auth: Authentication information
+     - parameter docType: Document type for which PDF should be loaded
+     - parameter id: Id of document
+     - parameter meta: Document template metadata
+     - returns: Observable sequence with http link to PDF document
+     */
+    public static func documentFromTemplate<T: MSGeneralDocument>(auth: Auth, docType: T.Type, id: String, meta: MSMeta) -> Observable<String> {
+        let urlPathComponents: [String] = [id, "export"]
+        var body = meta.dictionaryForTemplate()
+        body["extension"] = "pdf"
+        return HttpClient.updateWithHeadersResult(loadUrl(type: docType), auth: auth, urlPathComponents: urlPathComponents, body: body).flatMapLatest { result -> Observable<String> in
+            
+            guard let result = result else {
+                return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrecDocumentFromTemplateResponse.value))
+            }
+            guard let res = result["Location"] else {
+                return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrecDocumentFromTemplateResponse.value))
+            }
+            return Observable.just(res)
+        }
+    }
+    
+    /**
+     Load link to created publication for document from template.
+     Also see [ API reference](https://online.moysklad.ru/api/remap/1.1/doc/index.html#публикация-документов)
+     - parameter auth: Authentication information
+     - parameter docType: Document type for which publication should be loaded
+     - parameter id: Id of document
+     - parameter meta: Document template metadata
+     - returns: Observable sequence with http link to publication
+     */
+    public static func publicationFromTemplate<T: MSGeneralDocument>(auth: Auth, docType: T.Type, id: String, meta: MSMeta) -> Observable<String> {
+        let urlPathComponents: [String] = [id, "publication"]
+        let body = meta.dictionaryForTemplate()
+        return HttpClient.create(loadUrl(type: docType), auth: auth, urlPathComponents: urlPathComponents, body: body, contentType: .json).flatMapLatest { result -> Observable<String> in
+            guard let result = result else {
+                return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrecPublicationFromTemplateResponse.value))
+            }
+            guard let res = result["href"] as? String else {
+                return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrecPublicationFromTemplateResponse.value))
+            }
+            return Observable.just(res)
+        }
+    }
+    
+    /**
+     Load file disk
+     - parameter url: URL of file that should be loaded
+     - returns: Observable sequence with URL to downloaded file
+     */
+    public static func downloadDocument(url: URL) -> Observable<URL> {
+        return HttpClient.resultCreateFromData(url).flatMapLatest{ result -> Observable<URL> in
+            guard let result = result else {
+                return Observable.error(MSError.genericError(errorText: LocalizedStrings.incorrecDownloadDocumentResponse.value))
+            }
+            return Observable.just(result)
+        }
+    }
+}

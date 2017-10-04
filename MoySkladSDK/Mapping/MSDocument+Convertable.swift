a397e48d953567cd6bba98473b423f27d208625b
Vladislav Ivanov <vladislav.ivanov.v@gmail.com>
2017-10-04 15:51:40 +0300

    MI-946

--- MoySkladSDK/Mapping/MSDocument+Convertable.swift
+++ MoySkladSDK/Mapping/MSDocument+Convertable.swift
@@ -76,6 +76,8 @@ extension MSDocument : DictConvertable {
                    proceedsNoCash: Money(minorUnits: dict.value("proceedsNoCash") ?? 0),
                    proceedsCash: Money(minorUnits: dict.value("proceedsCash") ?? 0),
                    receivedNoCash: Money(minorUnits: dict.value("receivedNoCash") ?? 0),
-                   receivedCash: Money(minorUnits: dict.value("receivedCash") ?? 0)))
+                   receivedCash: Money(minorUnits: dict.value("receivedCash") ?? 0),
+                   customerOrders: dict.msArray("customerOrders").map { MSDocument.from(dict: $0) }.flatMap { $0 },
+                   supplies: dict.msArray("supplies").map { MSDocument.from(dict: $0)?.value() }.removeNils()))
     }
 }

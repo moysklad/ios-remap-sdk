e7fe89b384ecfc123a28d0571e7017a484e1d619
Anton Efimenko <aefimenko@moysklad.ru>
2017-11-10 14:44:12 +0300

    MI-981

--- MoySkladSDK/Mapping/Serialization/Assortment+Serialize.swift
+++ MoySkladSDK/Mapping/Serialization/Assortment+Serialize.swift
@@ -166,10 +166,15 @@ extension MSBundleComponent {
     public func dictionary(metaOnly: Bool = true) -> Dictionary<String, Any> {
         var dict = [String: Any]()
         
-        dict["meta"] = meta.dictionary()
+        if meta.href.count > 0 {
+            dict["meta"] = meta.dictionary()
+        }
+
         guard !metaOnly else { return dict }
         
-        dict.merge(id.dictionary())
+        if meta.href.count > 0 {
+            dict.merge(id.dictionary())
+        }
         
         dict["accountId"] = accountId
         dict["quantity"] = quantity

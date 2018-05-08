24fafc3effecf49c866f3f23bd1fed370b349570
Anton Efimenko <aefimenko@moysklad.ru>
2018-05-08 13:00:26 +0300

    MI-623

--- MoySkladSDK/Utils/Extensions.swift
+++ MoySkladSDK/Utils/Extensions.swift
@@ -278,7 +278,7 @@ public extension Date {
     
     public static var msDisplayLongDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        formatter.dateFormat = "dd.MM.yyyy HH:mm"
         formatter.locale = Locale(identifier: "ru_RU")
         return formatter
     }()
